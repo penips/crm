8dcf9c10d7a0f30581d584008491ad26fd137ab7
penips <hurricane.season.is.over@gmail.com>
2025-11-21 10:58:14 +1300

    renamed to Add Deal for consistency

--- src/app/pipeline/page.tsx
+++ src/app/pipeline/page.tsx
@@ -49,7 +49,7 @@ export default function PipelinePage() {
                 </div>
                 <Button onClick={() => setShowForm(true)}>
                     <Plus className="mr-2 h-4 w-4" />
-                    New Deal
+                    Add Deal
                 </Button>
             </div>
 
